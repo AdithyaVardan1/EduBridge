7b55ee0c97e44a44c039e4afa3b9d4739699b8c5
Adithya Vardan M <115080544+AdithyaVardan1@users.noreply.github.com>
2024-02-23 09:07:15 +0530

    Update phi.py

--- phi.py
+++ phi.py
@@ -3,20 +3,15 @@ import intel_extension_for_pytorch as ipex
 import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# Set device to CPU as IPEX optimizations are for Intel CPUs
 device = torch.device("cpu")
-start_time  = time.time()
-# Enable IPEX optimizations
+start_time = time.time()
+
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True).to(ipex.DEVICE)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
-# Take prompt as input from the user
 user_prompt = input("Enter your prompt: ")
 
-# Prepare inputs with tokenizer
 inputs = tokenizer(user_prompt, return_tensors="pt", return_attention_mask=False)
-
-# Since the model is already on the IPEX device, inputs need to be converted to IPEX tensors as well
 inputs = inputs.data.to(ipex.DEVICE)
 
 outputs = model.generate(
@@ -24,12 +19,12 @@ outputs = model.generate(
     max_length=500,
     do_sample=True,
     top_p=0.92,
-    temperature=0.85
+    top_k=50,  # Adjusted parameter
+    temperature=0.75  # Slightly lower temperature for more focus
 )
 
 end_time = time.time()
 print(f"Inference Time: {end_time - start_time} seconds")
 
-# Decode and print the output text
 text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(text)
