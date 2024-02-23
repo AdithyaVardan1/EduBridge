ba1ea482cd66276c72be33d42430c403cfa5aaf0
Adithya Vardan M <115080544+AdithyaVardan1@users.noreply.github.com>
2024-02-23 09:10:07 +0530

    Update phi.py

--- phi.py
+++ phi.py
@@ -3,24 +3,27 @@ import intel_extension_for_pytorch as ipex
 import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# Set device to CPU as optimizations are for Intel CPUs
 device = torch.device("cpu")
 start_time = time.time()
 
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True).to(ipex.DEVICE)
+# Enable IPEX optimizations by simply using .to(device) as you normally would
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True).to(device)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
 user_prompt = input("Enter your prompt: ")
 
 inputs = tokenizer(user_prompt, return_tensors="pt", return_attention_mask=False)
-inputs = inputs.data.to(ipex.DEVICE)
+
+# Move input tensors to the same device as the model
+inputs = {k: v.to(device) for k, v in inputs.items()}
 
 outputs = model.generate(
     **inputs,
     max_length=500,
     do_sample=True,
     top_p=0.92,
-    top_k=50,  # Adjusted parameter
-    temperature=0.75  # Slightly lower temperature for more focus
+    temperature=0.85
 )
 
 end_time = time.time()
